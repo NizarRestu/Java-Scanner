be3dea65e3a5c96bacdfd13ae012deecda05cca6
WINDOWS <nizarrestuaji18@gmail.com>
2022-11-09 09:55:05 +0700

    Edit

--- TugasJavaBasic.java
+++ TugasJavaBasic.java
@@ -151,7 +151,7 @@ public class TugasJavaBasic {
                                                 System.out.println("Bola");
                                                 System.out.print("Masukan Jari jari: ");
                                                 r = tugas.nextInt();
-                                                hasil = 4 / 3 * π * r * r * r;
+                                                hasil = 4 / 3 * 22 * r * r * r / 7;
                                                 System.out.print("Hasilnya: " + hasil);
                                                 System.out.print(" liter \n");
                                                 break;
